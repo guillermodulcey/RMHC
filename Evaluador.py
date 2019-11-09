ddbf1ecebe828a02447d7a6a6e9d5129961291f7
Hernán Guillermo Dulcey Morán <guille.dulcey@gmail.com>
2019-11-09 11:47:25 -0600

    Implementación de rangos
    Se implementa la posibilidad de escoger rangos.
    
    También se implementa la posibilidad de manejar el problema como maximización o minimización.

--- Evaluador.py
+++ Evaluador.py
@@ -1,15 +1,22 @@
 from Funcion import Funcion
+
+import sys as s
 import math as m
 
 class Evaluador():
-    def __init__(self, longitudEntera, longitud):
+    def __init__(self, longitudEntera, longitud, rangoInicial, rangoFinal, maximizar):
         self.longitudEntera = longitudEntera
         self.longitud = longitud
+        self.rangoInicial = rangoInicial
+        self.rangoFinal = rangoFinal
+        self.maximizar = maximizar
 
     def calcularFitness(self,arreglo):
         parteEntera = 0
+        contador = self.longitudEntera - 1
         for i in range(0,self.longitudEntera):
-            parteEntera += m.pow(2,i) * arreglo[i]
+            parteEntera += m.pow(2,contador) * arreglo[i]
+            contador -= 1
 
         parteDecimal = 0
         contador = 1
@@ -18,4 +25,20 @@ class Evaluador():
             contador += 1
 
         numero = parteEntera+parteDecimal
-        return numero
+        
+        if self.maximizar:
+            if self.verificarRango(numero):
+                return Funcion.funcion(numero)
+            else:
+                return s.float_info.min
+        else:
+            if self.verificarRango(numero):
+                return -Funcion.funcion(numero)
+            else:
+                return -s.float_info.max
+
+    def verificarRango(self, numero):
+        if numero < self.rangoInicial or numero > self.rangoFinal:
+            return False
+        else:
+            return True
--- RMHC.py
+++ RMHC.py
@@ -1,17 +1,21 @@
 from Individuo import Individuo
 from Evaluador import Evaluador
 
+import math as m
 import random as r
 import copy as c
 
 class RMHC():
 
-    def __init__(self, longitudEntera ,longIndividuo, semilla, probabilidad):
+    def __init__(self, longitudEntera ,longIndividuo, semilla, probabilidad, maximizar, rangoInicial, rangoFinal):
         r.seed(semilla)
 
         self.individuo = Individuo(longIndividuo, semilla)
         self.probabilidad = probabilidad
-        self.e = Evaluador(longitudEntera,longIndividuo)
+        self.maximizar = maximizar
+
+        self.e = Evaluador(longitudEntera,longIndividuo, rangoInicial, rangoFinal, maximizar)
+        
         self.best = self.e.calcularFitness(self.individuo.arreglo)
 
     def mutar(self):
@@ -24,9 +28,8 @@ class RMHC():
                 self.individuo.arreglo = mutacion
 
 for s in range(0,100):
-    x = RMHC(0,10,s,1)
+    x = RMHC(10,10,s,1,False,1,7)
     for i in range(0,10000):
         x.mutar()
     print("Mejor: "+str(x.e.calcularFitness(x.individuo.arreglo)))
     print("ARREGLO: " + str(x.individuo.arreglo))
-    
