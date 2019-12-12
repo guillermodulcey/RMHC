2047f9890bf5784538bc5ea12af54cfc4e37a56b
Karla Guzmán <karla_g97@hotmail.com>
2019-12-11 19:00:10 -0600

    Implementación Web
    Se implementa la aplicación utilizando flask.

--- Implementacion Edwin/FactoryPatternDemo.py
+++ Implementacion Edwin/FactoryPatternDemo.py
@@ -12,7 +12,9 @@ precision = 10
 semilla = 2
 iteraciones = 10000
 
+null 
+
 for i in range(0,10):
     semilla = i
     print(str(hf.getHeuristic("RMHC", False).execute(precision,semilla,iteraciones)))
-    print(str(hf.getHeuristic("ECLECTIC", False, 200).execute(precision,semilla,iteraciones)))
+    print(str(hf.getHeuristic("ECLECTIC", False, null).execute(precision,semilla,iteraciones)))
